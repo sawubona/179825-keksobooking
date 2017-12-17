0586fa9108814bf9752b20a95682019d5126c167
sawubona <sawubona36@gmail.com>
2017-12-17 21:46:41 +0300

    Выполнил задание №18

--- js/map.js
+++ js/map.js
@@ -1,8 +1,44 @@
 'use strict';
 
-(() => {
+window.map = (() => {
+  const dragObj = (target) => {  //migrate to mod drag.js
+    target.addEventListener('mousedown', function (event) {
+      let targetStartCords = {
+        x: event.clientX,
+        y: event.clientY
+      };
+
+      const onMouseMove = (moveEvent) => {
+        moveEvent.preventDefault();
+        let shift = {
+          x: targetStartCords.x - moveEvent.clientX,
+          y: targetStartCords.y - moveEvent.clientY
+        }
+        targetStartCords = {
+          x: moveEvent.clientX,
+          y: moveEvent.clientY
+        }
+        target.style.top = `${window.utils.targetDragLimiter(target.offsetTop, shift.y, 100, 500)}px`;
+        target.style.left = `${target.offsetLeft - shift.x}px`;
+      };
+
+      const onMouseUp = (upEvent) => {
+        address.value = `x: {${target.offsetLeft - (target.offsetWidth / 2)}}, y: {${target.offsetTop + target.offsetHeight}}`;
+        upEvent.preventDefault();
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
+  };
+
   window.mapdata.map.addEventListener('mouseup', window.pin.mainpinMouseHandler);
   window.mapdata.map.addEventListener('keydown', window.pin.mainpinTabHandler);
   window.mapdata.noticeForm.addEventListener('mouseup', window.formaction.formHandler);
   window.mapdata.noticeForm.addEventListener('submit', window.formaction.formValid);
+
+  return { dragButton: dragButton };
+
 })();
