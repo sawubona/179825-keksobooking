acc525d97c084e818514d8fb63cc484f0b90ab42
MaxFedotoff <sawubona36@gmail.com>
2017-12-20 11:33:52 +0300

    Внес исправления v.2

--- js/form.js
+++ js/form.js
@@ -5,16 +5,16 @@ window.formaction = (() => {
   const changeHandler = (event) => {
     let target = event.target;
     if (target.id === 'type') {
-      window.syncronizeField.syncroPrice(target, price, 'minimum', { 0: 1000, 1: 0, 2: 5000, 3: 10000 });
+      window.syncronizeField.syncroField(target, price, syncroPrice);
     };
     if (target.id === 'timein') {
-      window.syncronizeField.syncroTime(target, timeout);
+      window.syncronizeField.syncroField(target, timeout, syncroTime);
     };
     if (target.id === 'timeout') {
-      window.syncronizeField.syncroTime(target, timein);
+      window.syncronizeField.syncroField(target, timein, syncroTime);
     };
     if (target.id === 'room_number') {
-      window.syncronizeField.syncroRoom(target, capacity);
+      window.syncronizeField.syncroField(target, capacity, syncroRoom);
     };
   };
 
@@ -40,6 +40,70 @@ window.formaction = (() => {
       if (element.id === 'price' && element.value.length < 0) {
         element.style.border = '2px solid red';
         event.preventDefault();
+      }
+    }
+  };
+
+  /*----------------------*/
+
+  const syncroRoom = (master, slave) => {
+    slave.selectedIndex = -1;
+    for (let i = 0; i < master.options.length; i++) {
+      const element = master.options[i];
+      if (element.selected) {
+        if (element.index === 0) {
+          slave.selectedIndex = 2;
+          slave.options[0].setAttribute('disabled', '');
+          slave.options[1].setAttribute('disabled', '');
+          slave.options[3].setAttribute('disabled', '');
+          slave.options[2].removeAttribute('disabled', '');
+        }
+        if (element.index === 1) {
+          slave.selectedIndex = 1;
+          slave.options[0].setAttribute('disabled', '');
+          slave.options[3].setAttribute('disabled', '');
+          slave.options[2].removeAttribute('disabled', '');
+          slave.options[1].removeAttribute('disabled', '');
+        }
+        if (element.index === 2) {
+          slave.selectedIndex = 0;
+          slave.options[3].setAttribute('disabled', '');
+          slave.options[0].removeAttribute('disabled', '');
+          slave.options[1].removeAttribute('disabled', '');
+          slave.options[2].removeAttribute('disabled', '');
+        }
+        if (element.index === 3) {
+          slave.selectedIndex = 3;
+          slave.options[0].setAttribute('disabled', '');
+          slave.options[1].setAttribute('disabled', '');
+          slave.options[2].setAttribute('disabled', '');
+          slave.options[3].removeAttribute('disabled', '');
+        }
+      }
+    }
+  };
+
+  const syncroPrice = (master, slave) => {
+    if (master.selectedIndex === 0) {
+      slave.min = 1000;
+    }
+    if (master.selectedIndex === 1) {
+      slave.min = 0;
+    }
+    if (master.selectedIndex === 2) {
+      slave.min = 5000;
+    }
+    if (master.selectedIndex === 3) {
+      slave.min = 10000;
+    }
+  };
+
+  const syncroTime = (master, slave) => {
+    slave.selectedIndex = -1;
+    for (let i = 0; i < master.options.length; i++) {
+      const element = master.options[i];
+      if (element.selected) {
+        slave.selectedIndex = element.index;
       }
     }
   };
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -2,71 +2,10 @@
 
 window.syncronizeField = (() => {
 
-  const syncroRoom = (master, slave) => {
-    slave.selectedIndex = -1;
-    for (let i = 0; i < master.options.length; i++) {
-      const element = master.options[i];
-      if (element.selected) {
-        if (element.index === 0) {
-          slave.selectedIndex = 2;
-          slave.options[0].setAttribute('disabled', '');
-          slave.options[1].setAttribute('disabled', '');
-          slave.options[3].setAttribute('disabled', '');
-          slave.options[2].removeAttribute('disabled', '');
-        }
-        if (element.index === 1) {
-          slave.selectedIndex = 1;
-          slave.options[0].setAttribute('disabled', '');
-          slave.options[3].setAttribute('disabled', '');
-          slave.options[2].removeAttribute('disabled', '');
-          slave.options[1].removeAttribute('disabled', '');
-        }
-        if (element.index === 2) {
-          slave.selectedIndex = 0;
-          slave.options[3].setAttribute('disabled', '');
-          slave.options[0].removeAttribute('disabled', '');
-          slave.options[1].removeAttribute('disabled', '');
-          slave.options[2].removeAttribute('disabled', '');
-        }
-        if (element.index === 3) {
-          slave.selectedIndex = 3;
-          slave.options[0].setAttribute('disabled', '');
-          slave.options[1].setAttribute('disabled', '');
-          slave.options[2].setAttribute('disabled', '');
-          slave.options[3].removeAttribute('disabled', '');
-        }
-      }
-    }
+  const syncroField = (target, source, callback) => {
+    callback(target, source);
   };
 
-  const syncroPrice = (master, slave, attr, obj) => {
-    for (let key in obj) {
-      let value = obj[key];
-      if (master.selectedIndex === +key) {
-        if (attr === 'minimum') {
-          slave.min = value;
-        }
-        if (attr === 'maximum') {
-          slave.max = value;
-        }
-      }
-    }
-  };
-
-  const syncroTime = (master, slave) => {
-    slave.selectedIndex = -1;
-    for (let i = 0; i < master.options.length; i++) {
-      const element = master.options[i];
-      if (element.selected) {
-        slave.selectedIndex = element.index;
-      }
-    }
-  };
-
-  return {
-    syncroRoom: syncroRoom,
-    syncroPrice: syncroPrice,
-    syncroTime: syncroTime
-  };
+  return { syncroField: syncroField };
 
 })();
